586cdfa17f7ff72fae47191a97c1081e4bb739b3
Yacine31 <yacine.oumghar@gmail.com>
2021-11-25 03:25:48 +0100

    Update redolog_size_per_days.sql

--- redolog_size_per_days.sql
+++ redolog_size_per_days.sql
@@ -2,8 +2,9 @@ REM
 REM Tailes des redoslog par jour
 REM 
 
+set pages 0
 select 
-	to_char(first_time, 'YYYY/MM/dd'),
+	to_char(first_time, 'YYYY/MM/dd') "Jour",
 	ROUND(sum(BLOCKS*BLOCK_SIZE)/1024/1024, 0) "Taille_Mo"
 from v$archived_log
 group by to_char(first_time, 'YYYY/MM/dd')
