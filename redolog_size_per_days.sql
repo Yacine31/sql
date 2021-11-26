24c8042c62da89031dccf0f1854bf5620b628912
Yacine31 <yacine.oumghar@gmail.com>
2021-11-26 07:19:46 +0100

    Update redolog_size_per_days.sql

--- redolog_size_per_days.sql
+++ redolog_size_per_days.sql
@@ -4,6 +4,7 @@ REM
 
 select 
 	to_char(first_time, 'YYYY/MM/dd') "Jour",
+    count(*) "Nbr de fichiers",
 	ROUND(sum(BLOCKS*BLOCK_SIZE)/1024/1024, 0) "Taille_Mo"
 from v$archived_log
 group by to_char(first_time, 'YYYY/MM/dd')
