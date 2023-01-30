5b319d6006118d9eae0209eb27660742c5413e46
Yacine31 <yacine.oumghar@gmail.com>
2023-01-30 19:46:56 +0100

    Update rapport_html.sql

--- Daily/rapport_html.sql
+++ Daily/rapport_html.sql
@@ -7,11 +7,12 @@ set feedback off
 
 
 SET MARKUP HTML ON SPOOL ON PREFORMAT OFF ENTMAP OFF -
-HEAD "<TITLE>Database Report</TITLE> -
-<STYLE type='text/css'> -
-</STYLE>" -
-BODY "TEXT='#FF00Ff'" -
-TABLE "WIDTH='90%' BORDER='1'"
+
+HEAD "<title>Rapport</title>
+    <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css rel=stylesheet integrity=sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD crossorigin=anonymous>
+  </head>
+  <body>
+   <div class=container>" 
 
 -- ---------------------------------------------------
 prompt <h2>Current DATE</h2>
