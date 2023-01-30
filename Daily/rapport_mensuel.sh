2e04071609e10f35d23f66ceb57b2fdea43755f3
Yacine31 <yacine.oumghar@gmail.com>
2023-01-30 19:32:29 +0100

    renomage

--- Daily/rapport_mensuel.sh
+++ Daily/rapport_mensuel.sh
@@ -1,10 +1,11 @@
 DATETIME=`date +%Y%m%d%H%M`
+HNAME=$(hostname)
 
 for r in $(ps -eaf | grep pmon | egrep -v 'grep|ASM1|APX1' | cut -d '_' -f3)
 do
         export ORAENV_ASK=NO
         export ORACLE_SID=$r
         . oraenv -s > /dev/null
-        sqlplus -s "/ as sysdba" @DailyCheck_html.sql > Rapport_${ORACLE_SID}_${DATETIME}.html
-        echo Rapport dans le fichier html : Rapport_${ORACLE_SID}_${DATETIME}.html
+        sqlplus -s "/ as sysdba" @rapport_html.sql > Rapport_${ORACLE_SID}_${DATETIME}.html
+        echo Rapport dans le fichier html : Rapport_$HNAME_${ORACLE_SID}_${DATETIME}.html
 done
