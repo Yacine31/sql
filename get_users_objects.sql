6dc2ad67a9a2e4be5a38e4f62531317a7e1db0ee
Yacine31 <yacine.oumghar@gmail.com>
2023-05-05 22:50:59 +0200

    Create get_users_objects.sql

---
+++ get_users_objects.sql
@@ -0,0 +1,21 @@
+set pages 999 lines 200
+col owner for a30
+
+select * from
+(select owner, object_type ,count(*) as object_count from dba_objects group by owner, object_type order by owner, object_type)
+  pivot
+  (
+     max(object_count)
+     for object_type in (
+             'TABLE',
+             'VIEW',
+             'INDEX',
+             'FUNCTION',
+             'LOB',
+             'PACKAGE',
+             'PROCEDURE',
+             'TRIGGER',
+             'SYNONYM'
+        )
+  )
+  order by owner;
