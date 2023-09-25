ae3cfd1f1aa0138146488eaf5455a7ebd06df109
Yacine31 <yacine.oumghar@gmail.com>
2023-09-25 21:01:10 +0200

    Update backup_rman_all_db.sh

--- backup_rman_all_db.sh
+++ backup_rman_all_db.sh
@@ -9,5 +9,5 @@ export SCRIPTS_DIR=/home/oracle/scripts
 
 for b in $(ps -ef | grep pmon | grep -v grep | cut -d_ -f3 | sort)
 do 
-	${SCRIPTS_DIR}/backup_rman.sh -s $b -t DB
+	${SCRIPTS_DIR}/backup_rman.sh $b
 done
