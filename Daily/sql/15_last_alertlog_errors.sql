a5630b151f9f5b84a20288310600a612620ef0cc
Yacine31 <yacine.oumghar@gmail.com>
2023-11-06 19:15:16 +0100

    numerotation

---
+++ Daily/sql/15_last_alertlog_errors.sql
@@ -0,0 +1,5 @@
+set pages 999 lines 150
+select to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY HH-MM-SS') || ' : ' || message_text "Last alertlog (30 days)"
+FROM X$DBGALERTEXT
+WHERE originating_timestamp > systimestamp - 30  AND regexp_like(message_text, '(ORA-)');
+exit
---
+++ Daily/sql/17_last_rman_backup.sql
@@ -0,0 +1,20 @@
+alter session set nls_date_format='DD/MM/YYYY HH24:MI:SS' ;
+set linesize 250 heading off;
+set heading on pagesize 999;
+column status format a25;
+column input_bytes_display format a12;
+column output_bytes_display format a12;
+column device_type format a10;
+
+select
+        b.input_type,
+        b.status,
+        to_char(b.start_time,'DD-MM-YYYY HH24:MI') "Start Time",
+        to_char(b.end_time,'DD-MM-YYYY HH24:MI') "End Time",
+        b.output_device_type device_type,
+        b.input_bytes_display,
+        b.output_bytes_display
+FROM v$rman_backup_job_details b
+WHERE b.start_time > (SYSDATE - 30)
+ORDER BY b.start_time asc;
+exit
