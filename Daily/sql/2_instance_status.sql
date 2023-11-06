1592e22879acb994dc1ab05372cc9a5dfb4d0664
Yacine31 <yacine.oumghar@gmail.com>
2023-11-06 18:13:48 +0100

    Update 2_instance_status.sql

--- Daily/sql/2_instance_status.sql
+++ Daily/sql/2_instance_status.sql
@@ -1,4 +1,5 @@
-select inst_id,
+select 
+ inst_id,
  instance_name,
  status,
  VERSION_FULL,
@@ -6,8 +7,6 @@ select inst_id,
  ARCHIVER,
  INSTANCE_ROLE,
  database_status,
- active_state,
- to_char(startup_time,'DD/MM/YYYY HH24:MI:SS') startup_time 
 FROM  gv$instance;
 
 SELECT inst_id, name, to_char(CREATED ,'DD/MM/YYYY') CREATED , open_mode, DATABASE_ROLE, log_mode, FORCE_LOGGING, CURRENT_SCN FROM gv$database;
