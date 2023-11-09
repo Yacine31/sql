a3fe06d96e1deef7c8a46df3918e4f4c951922a0
Yacine31 <yacine.oumghar@gmail.com>
2023-11-09 02:44:06 +0100

    Update 10_get_disk_size.sql

--- Daily/sql/10_get_disk_size.sql
+++ Daily/sql/10_get_disk_size.sql
@@ -1,6 +1,14 @@
-prompt <h2>Disk Size </h2>
+prompt <h2>Disk and Memory Size </h2>
 set echo off head off
 prompt <pre>
 host df -h
+prompt </pre>
+
+prompt <pre>
+lsblk -f
+prompt </pre>
+
+prompt <pre>
+free -h
 prompt </pre>
 exit
