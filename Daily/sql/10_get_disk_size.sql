fc8ecf870d125590a2ca3f79a2d08b7a42d30b74
Yacine31 <yacine.oumghar@gmail.com>
2023-11-09 02:49:05 +0100

    Update 10_get_disk_size.sql

--- Daily/sql/10_get_disk_size.sql
+++ Daily/sql/10_get_disk_size.sql
@@ -1,21 +1,25 @@
-prompt <h2>Disk and Memory Size </h2>
+prompt <h2>Disk Size df -h</h2>
 set echo off head off
 prompt <pre>
 host df -h
 prompt </pre>
 
+prompt <h2>lsblk -f </h2>
 prompt <pre>
 host lsblk -f
 prompt </pre>
 
+prompt <h2>cat /etc/fstab </h2>
 prompt <pre>
 host cat /etc/fstab
 prompt </pre>
 
+prompt <h2>Memory Size free -h </h2>
 prompt <pre>
 host free -h
 prompt </pre>
 
+prompt <h2>lscpu </h2>
 prompt <pre>
 host lscpu
 prompt </pre>
