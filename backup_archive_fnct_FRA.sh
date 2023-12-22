f90dddc64c27e19dc0ed87725d87c3b112917f78
Yacine31 <yacine.oumghar@gmail.com>
2023-12-22 19:27:00 +0100

    Update backup_archive_fnct_FRA.sh

--- backup_archive_fnct_FRA.sh
+++ backup_archive_fnct_FRA.sh
@@ -15,7 +15,21 @@ exit $1
 } #f_help
 
 #------------------------------------------------------------------------------
+# fonction de récupération de la taille FRA utilisée
+#------------------------------------------------------------------------------
 
+f_fra_size() {
+
+fra_usage=$(sqlplus -s '/ as sysdba' << EOF
+    set pages 0 feedback off;
+    select round(sum(percent_space_used),0) from v\$flash_recovery_area_usage;
+EOF
+)
+
+return ${fra_usage}
+}
+
+#------------------------------------------------------------------------------
 ORACLE_SID=$1
 
 [ "${ORACLE_SID}" ] || f_help 2;
@@ -26,10 +40,4 @@ ORAENV_ASK=NO
 PATH=/usr/local/bin:$PATH
 . oraenv -s >/dev/null
 
-fra_usage=$(sqlplus -s '/ as sysdba' << EOF
-    set pages 0 feedback off;
-    select round(sum(percent_space_used),0) from v\$flash_recovery_area_usage;
-EOF
-)
-
-return ${fra_usage}
+f_fra_size ${ORACLE_SID}
