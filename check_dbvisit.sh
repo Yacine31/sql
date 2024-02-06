9c55b4b544ae444a58a5bb2c90683a5d6519fd75
Yacine31 <yacine.oumghar@gmail.com>
2024-02-06 02:49:32 +0100

    Update check_dbvisit.sh

--- check_dbvisit.sh
+++ check_dbvisit.sh
@@ -19,17 +19,17 @@ apply_log_gap=$(echo "$output" | awk '/Apply Log Gap/{print $4}')
 # Test pour Transfer Log Gap
 if [ "$transfer_log_gap" -gt 10 ]; then
     # Afficher le message d'alerte avec la valeur actuelle
-    echo "Alerte : La valeur de Transfer Log Gap ($transfer_log_gap) pour $ORCL est supérieure à 10."
+    echo "Alerte : La valeur de Transfer Log Gap ($transfer_log_gap) pour $DBNAME est supérieure à 10."
 else
     # Afficher un message indiquant que tout est OK
-    echo "La valeur de Transfer Log Gap pour $ORCL est inférieure ou égale à 10."
+    echo "La valeur de Transfer Log Gap pour $DBNAME est inférieure ou égale à 10."
 fi
 
 # Test pour Apply Log Gap
 if [ "$apply_log_gap" -gt 10 ]; then
     # Afficher le message d'alerte avec la valeur actuelle
-    echo "Alerte : La valeur de Apply Log Gap ($apply_log_gap) pour $ORCL est supérieure à 10."
+    echo "Alerte : La valeur de Apply Log Gap ($apply_log_gap) pour $DBNAME est supérieure à 10."
 else
     # Afficher un message indiquant que tout est OK
-    echo "La valeur de Apply Log Gap pour $ORCL est inférieure ou égale à 10."
+    echo "La valeur de Apply Log Gap pour $DBNAME est inférieure ou égale à 10."
 fi
