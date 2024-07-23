9b2f0ceaa70a82062cdd0c368da8fe4ace9afd74
Yacine31 <yacine.oumghar@gmail.com>
2024-07-23 15:34:51 +0200

    Update export_datapump_alldb.sh
    exclusion de +ASM et +APX

--- export_datapump_alldb.sh
+++ export_datapump_alldb.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #------------------------------------------------------------------------------
-# ORACLE DATABASE : EXPDP ALL DB RMAN
+# ORACLE DATABASE : EXPDP ALL DB 
 #------------------------------------------------------------------------------
 # Historique :
-#       21/04/2023 : YAO - Creation : export datapump de toutes les bases ouvertes
+#       21/04/2023 : YOU - Creation : export datapump de toutes les bases ouvertes
 #------------------------------------------------------------------------------
 
-# toutes les bases ouvertes sont sauvegardées par le script RMAN
+# toutes les bases ouvertes sont sauvegardées par le expdp
 
-for i in $(ps -ef | grep pmon | grep -v grep | cut -d_ -f3)
+for i in $(ps -ef | grep pmon | grep -v grep | cut -d_ -f3 | egrep -v '+ASM|+APX)
 do
         sh /home/oracle/scripts/export_datapump.sh $i
 done
