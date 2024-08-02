f6b518718bf012b9242b2f892ac408dd3d031535
Yacine31 <yacine.oumghar@gmail.com>
2024-08-02 14:45:27 +0200

    Update backup_bin.sh

--- backup_bin.sh
+++ backup_bin.sh
@@ -38,9 +38,9 @@ cd ${BKP_APP_LOCATION}
 # suppression des anciennes sauvegardes
 rm -fv backup_bin_oraapp_*.tgz
 # compression du repertoire oracle app avec exclusion des répertoires admin, diag et audit
-sudo tar --exclude='${ORA_APP_LOCATION}/oracle/admin' \
---exclude='${ORA_APP_LOCATION}/oracle/audit' \
---exclude='${ORA_APP_LOCATION}/oracle/diag' \
+sudo tar --exclude="${ORA_APP_LOCATION}/oracle/admin" \
+--exclude="${ORA_APP_LOCATION}/oracle/audit" \
+--exclude="${ORA_APP_LOCATION}/oracle/diag" \
 cfz backup_bin_oraapp_$(date +%Y%m%d).tgz ${ORA_APP_LOCATION} 
 
 # notification
